7b18a149faf37d943f9aa92715ce332db3f201c2
Brahim HM <brahimhm98@gmail.com>
2023-09-06 19:59:01 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+
+const submitData=(name, email)=>{    
+      // method: "POST" is missing from the object below
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,}),
+      };
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(errorMessage => {
+                    throw new Error(errorMessage);
+                });
+            }
+
+            return response.json();
+
+        })
+        .then(responseData => {
+            const newId = responseData.id;
+            document.body.innerHTML += `<p>New ID: ${newId}</p>`;
+        })
+        .catch(error => {
+            document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+        });
+
+}
